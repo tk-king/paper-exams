8cf36873f577c195cf642cc4ae7709521f280328
Tobias King <tk.king.tk@gmail.com>
2024-07-23 00:17:00 +0200

    Working on styling

--- frontend/paper-exam/app/Login.js
+++ frontend/paper-exam/app/Login.js
@@ -9,14 +9,15 @@ const Login = () => {
     return <div>
         <h1>Login</h1>
         <Box sx={{ display: "flex", flexDirection: "column"}}>
-            <Input sx={{m: 2}} type="text" placeholder="Username" />
-            <Input sx={{m: 2}} type="password" placeholder="Password" />
+            <Input className="input-primary" sx={{m: 2}} type="text" placeholder="Username" />
+            <Input className="input-primary" sx={{m: 2}} type="password" placeholder="Password" />
             <Button>Login</Button>
         </Box>
         <hr></hr>
         <div>or</div>
         <h4>Login with a provider</h4>
         <Button
+            className="button-primary"
             onClick={() => auth.loginOAuth("github")}
         >Login with Github</Button>
     </div>
